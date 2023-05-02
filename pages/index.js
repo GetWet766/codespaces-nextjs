af1db3a07c70a443731410e578c5da9c1b5b93cd
GetWet766 <77196443+GetWet766@users.noreply.github.com>
2023-05-02 21:06:31 +0000

    Added title

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Window from '../components/Window/Window'
 import Search from '../components/Search/Search'
 import Weather from '../components/Weather/Weather'
 import getWeather from '../hooks/getWeather'
+import Head from 'next/head'
 import { useState, useEffect } from 'react'
 var used = false
 
@@ -21,6 +22,10 @@ function Home() {
   
   return (
     <Window>
+      <Head>
+        <title>Прогноз погоды</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Search setWeather={setWeather} inputCity={inputCity} setInputCity={setInputCity} setCity={setCity} setDegreesCelsias={setDegreesCelsias} />
       <Weather city={city} degreesCelsias={degreesCelsias} weather={weather} />
     </Window>
