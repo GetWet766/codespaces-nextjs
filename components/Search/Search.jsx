58ac5c432602627dc54d45e28d79e5df200b1db2
GetWet766 <77196443+GetWet766@users.noreply.github.com>
2023-05-02 19:38:31 +0000

    Success connect

--- components/Search/Search.jsx
+++ components/Search/Search.jsx
@@ -22,7 +22,7 @@ const Search = ({inputCity, setInputCity, setCity, setDegreesCelsias}) => {
 
 function searchSubmit(inputCity, setCity, setDegreesCelsias) {
     if (inputCity === "") return
-    const API_KEY = process.env.WEATHER_API
+    const API_KEY = "d551f5d509a0045cf8cdb61648300237"
     
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&appid=${API_KEY}`)
       .then(response => response.json())
