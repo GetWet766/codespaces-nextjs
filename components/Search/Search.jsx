37c09db6c4f1fce92dcd2ce3c634fa6a6e321e44
GetWet766 <77196443+GetWet766@users.noreply.github.com>
2023-05-02 16:10:34 +0000

    First version

---
+++ components/Search/Search.jsx
@@ -0,0 +1,21 @@
+import cls from './Search.module.css'
+import img from '../../img/search-icon.png'
+
+const Search = () => {
+    return (
+        <div className={cls.search}>
+            <input
+                type="text"
+                name="search"
+                className={cls.searchInput}
+                id="search-input"
+                placeholder="Поиск города"
+            />
+            <div className={cls.buttonSearch}>
+                <img src={img.src} alt="" />
+            </div>
+        </div>
+      )
+}
+
+export default Search
---
+++ components/Weather/Weather.jsx
@@ -0,0 +1,27 @@
+import cls from './Weather.module.css'
+
+import Title from '../Title/Title'
+import Degree from '../Degree/Degree'
+
+import imgWeather from '../../img/cloudy-icon.png'
+import navIcon from '../../img/navigation-arrow.png'
+
+const Weather = ({country}) => {
+    return (
+        <div className={cls.weather}>
+            <div className={cls.weatherImg}>
+                <img src={imgWeather} alt="" />
+            </div>
+            <Title>
+                {country}
+                <img className={cls.imgNavigation} src={navIcon} alt="" />
+            </Title>
+            <div className={cls.celsius}>
+                31
+                <Degree/>
+            </div>
+        </div>
+      )
+}
+
+export default Weather
