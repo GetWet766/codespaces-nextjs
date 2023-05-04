59011373473f572df2e75faa3ffd79bee3bd3082
GetWet766 <77196443+GetWet766@users.noreply.github.com>
2023-05-04 21:36:18 +0000

    Manifest add

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,15 @@ import Window from '../components/Window/Window'
 import Search from '../components/Search/Search'
 import Weather from '../components/Weather/Weather'
 
+import manifest from "../hooks/manifest";
+
 import getWeather from '../hooks/getWeather'
 
 import Head from 'next/head'
 
 import { useEffect, useState } from 'react'
 import { useGeolocated } from "react-geolocated";
+import { useRouter } from 'next/router';
 
 function Home() {
   const [inputCity, setInputCity] = useState("")
@@ -69,6 +72,7 @@ function Home() {
       <Head>
         <title>Прогноз погоды</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="manifest" href={"data:application/json;charset=utf-8," + encodeURIComponent(manifest())} />
       </Head>
       <Search setWeather={setWeather} inputCity={inputCity} setInputCity={setInputCity} setCity={setCity} setDegreesCelsias={setDegreesCelsias} setAutoLocation={setAutoLocation} setHumidity={setHumidity} setLocalTime={setLocalTime} setRain={setRain} />
       <Weather city={city} degreesCelsias={degreesCelsias} weather={weather} autoLocation={autoLocation} humidity={humidity} localTime={localTime} rain={rain} />
