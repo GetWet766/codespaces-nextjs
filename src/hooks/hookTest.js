eea3f8b91616623fe66dffe94f462411812a949c
GetWet766 <prokopevmisha766@gmail.com>
2023-05-05 14:15:35 +0300

    1test

--- src/hooks/hookTest.js
+++ src/hooks/hookTest.js
@@ -2,6 +2,8 @@ function searchSubmit({ inputCity, setCity, setDegreesCelsias, setWeather, lat, 
   if (inputCity === "" & lat === "" & lon === "") return
   const API_KEY = "d551f5d509a0045cf8cdb61648300237"
   
+
+  
   if (inputCity !== "" & lat === "" & lon === "")
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&lang=ru&appid=${API_KEY}`)
       .then(response => response.json())
